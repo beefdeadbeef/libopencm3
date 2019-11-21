8461ade8389929f83f321ea8f395f612e72d545d
vampirefrog <mihai.draghicioiu@gmail.com>
2019-11-21 20:11:21 +0200

    support and require fully const configuration descriptors, no more *extra stuff

--- include/libopencm3/usb/usbd.h
+++ include/libopencm3/usb/usbd.h
@@ -105,7 +105,7 @@ extern const usbd_driver lm4f_usb_driver;
  * while the second @e const refers to the array.
  */
 extern usbd_device *usbd_init(const usbd_driver *driver, const struct usb_device_descriptor *dev,
-	const struct usb_config_descriptor *conf, const char *const *strings, int num_strings, uint8_t *control_buffer,
+	const struct usb_config_descriptor **conf, const char *const *strings, int num_strings, uint8_t *control_buffer,
 	uint16_t control_buffer_size);
 
 /** Registers a reset callback */
--- lib/usb/usb_private.h
+++ lib/usb/usb_private.h
@@ -55,10 +55,25 @@ LGPL License Terms @ref lgpl_license
 #define ENDPOINT_COUNT 4U
 #endif
 
+/**
+ * Maximum number of interface for memory to allocate.
+ * The allocated memory is used keep track of the SET_INTERFACE setting
+ * If the value is 0, no memory is allocated
+ *
+ * @note If a value could not be stored
+ *  (due to no memory allocated OR allocate memory not enought to store the value),
+ * then the following behavour apply:
+ *  - SET_INTEFACE for other than alternate-setting = 0 will always result in STALL.
+ *  - GET_INTEFACE will always result in STALL
+ */
+#if !defined(USBD_INTERFACE_MAX)
+# define USBD_INTERFACE_MAX 8
+#endif
+
 /** Internal collection of device information. */
 struct _usbd_device {
 	const struct usb_device_descriptor *desc;
-	const struct usb_config_descriptor *config;
+	const struct usb_config_descriptor **config;
 	const usb_bos_descriptor *bos;
 	const char *const *strings;
 	int num_strings;
@@ -70,6 +85,8 @@ struct _usbd_device {
 	uint8_t current_config;
 
 	uint16_t pm_top; /**< Top of allocated endpoint buffer memory */
+
+	const struct usb_interface_descriptor *current_iface[USBD_INTERFACE_MAX];
 
 	/* User callback functions for various USB events */
 	void (*user_callback_reset)(void);
